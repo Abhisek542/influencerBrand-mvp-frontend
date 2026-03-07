699275f8394e82618b60346a50c9649958f8d780
Abhisek542 <boseabhisek76@gmail.com>
2026-03-07 17:33:00 +0530

    Commit for aceepted-changes integration

--- src/app/features/influencer/accepted-campaigns/accepted-campaigns.ts
+++ src/app/features/influencer/accepted-campaigns/accepted-campaigns.ts
@@ -1,15 +1,10 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { CampaignCard } from '../../../shared/components/campaign-card/campaign-card';
 import { EmptyState } from '../../../shared/components/empty-state/empty-state';
 import { CommonModule } from '@angular/common';
+import { Campaign } from '../../../core/models/campaign.model';
+import { InfluencerService } from '../../../core/services/influencer.service';
 
-interface AcceptedCampaign {
-  id: number;
-  title: string;
-  brand: string;
-  niche: string;
-  budget: string;
-}
 @Component({
   selector: 'app-accepted-campaigns',
   imports: [CampaignCard, EmptyState,CommonModule],
@@ -17,13 +12,43 @@ interface AcceptedCampaign {
   styleUrl: './accepted-campaigns.css',
 })
 export class AcceptedCampaigns {
-  acceptedCampaigns: AcceptedCampaign[] = [
-    {
-      id: 1,
-      title: 'YouTube Product Review',
-      brand: 'Boat',
-      niche: 'Tech',
-      budget: '₹10,000',
-    }
-  ];
+ 
+  acceptedCampaigns : Campaign[] = [];
+  loading = false;
+
+  constructor(private influencerService: InfluencerService, private cdr: ChangeDetectorRef) {}
+
+  ngOnInit() {
+    this.loadAcceptedCampaigns();
+  }
+
+  loadAcceptedCampaigns() {
+   
+    this.influencerService.getAcceptedCampaigns().subscribe({
+      next: (data) => {
+
+        console.log('Raw accepted campaigns data:', data); // Debug log
+        this.acceptedCampaigns = data.map(c => ({
+          id: c.id,
+          title: c.title,
+          description: c.description,
+          niche: c.niche,
+          budget: `₹${c.budget}`,
+          deadline: c.deadline,
+          brandName: c.brandName
+        }));
+
+        this.loading = false;  // VERY IMPORTANT
+        this.cdr.detectChanges();
+
+      },
+      error: (err) => {
+        console.error(err);
+        this.loading = false;  // ALSO IMPORTANT
+        this.cdr.detectChanges();
+      }
+
+    });
+
+  }
 }
